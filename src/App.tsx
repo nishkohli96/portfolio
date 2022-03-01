8464c4966582052b89373f6ce4508b2ee4ad34a5
Nishant Kohli <nishkohli96@gmail.com>
2022-03-01 15:40:51 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { Suspense } from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
-import { store, persistor } from '_Redux/index';
 import Loading from '_Shared/Loading';
 import Routing from '_Routes/index';
 
 function App() {
     return (
         <Suspense fallback={Loading}>
-            <Provider store={store}>
-                <PersistGate loading={<Loading />} persistor={persistor}>
-                    <Routing />
-                </PersistGate>
-            </Provider>
+            <Routing />
         </Suspense>
     );
 }
