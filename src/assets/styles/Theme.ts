1ca1981b44db737632ab48e735a840822064df47
Nishant Kohli <nishkohli96@gmail.com>
2022-03-05 20:05:29 +0530

    theming wip

--- src/assets/styles/Theme.ts
+++ src/assets/styles/Theme.ts
@@ -1,4 +1,4 @@
-import {  deepOrange, grey } from '@mui/material/colors';
+// import {  deepOrange, grey } from '@mui/material/colors';
 import { PaletteMode } from '@mui/material';
 
 export const getDesignTokens = (mode: PaletteMode) => ({
@@ -11,22 +11,22 @@ export const getDesignTokens = (mode: PaletteMode) => ({
                 main: '#1f1f1f',
             }),
         },
-        ...(mode === 'dark' && {
-            background: {
-                default: deepOrange[900],
-                paper: deepOrange[900],
-            },
-        }),
-        text: {
-            ...(mode === 'light'
-                ? {
-                      primary: '#abcded',
-                      secondary: grey[800],
-                  }
-                : {
-                      primary: '#007aba',
-                      secondary: grey[500],
-                  }),
-        },
+        // ...(mode === 'dark' && {
+        //     background: {
+        //         default: deepOrange[900],
+        //         paper: deepOrange[900],
+        //     },
+        // }),
+        // secondary: {
+        //     ...(mode === 'light'
+        //         ? {
+        //               primary: '#abcded',
+        //               secondary: grey[800],
+        //           }
+        //         : {
+        //               primary: '#007aba',
+        //               secondary: grey[500],
+        //           }),
+        // },
     },
 });
--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,16 +1,56 @@
 import { FC } from 'react';
-import { useHistory } from 'react-router-dom';
-import Button from '@mui/material/Button';
-
+import { Typography } from '@mui/material';
 const Page1: FC = () => {
-    const history = useHistory();
 
     return (
         <div className="root">
-            <div>hi from Page1. Edit the code</div>
-            <Button variant="outlined" onClick={() => history.push('/page-2')}>
-                Go to Page2
-            </Button>
+           <div className="grid grid-cols-1 xl:grid-cols-2 gap-2 pb-5">
+            <div className="col-span-1 mx-auto my-auto">
+                <Typography variant='h2'>
+                    Hi There&nbsp;
+                    <span role="img" aria-label="hi">
+                        👋
+                    </span>
+                </Typography>
+                <Typography variant='h4'>
+                    Nice to meet you.
+                </Typography>
+            </div>
+            <div className="mx-auto col-span-1 md:pt-10 respdiv">
+                <p className="text-lg lg:text-xl lg:leading-relaxed leading-8">
+                    Myself Nishant. I am a Self-taught&nbsp;
+                    <span role="img" aria-label="graduate">
+                        👨‍🎓
+                    </span>
+                    &nbsp;FullStack Developer with over three years of experience.
+                </p>
+                <p className="text-lg lg:text-xl lg:leading-relaxed mt-5 leading-8">
+                    My core skillset includes ReactJS and React-Native along
+                    with relevant experience in NodeJS, Express, Fastify,
+                    MongoDB etc. I love playing around with new libraries and
+                    frameworks.
+                </p>
+                <p className="text-lg lg:text-xl lg:leading-relaxed mt-5 leading-8">
+                    Besides that, I love travelling &nbsp;
+                    <span role="img" aria-label="travel">
+                        ✈
+                    </span>
+                    , watching movies &nbsp;
+                    <span role="img" aria-label="tv">
+                        📺
+                    </span>
+                    , listening music &#127925;, playing video games &nbsp;
+                    <span role="img" aria-label="games">
+                        🎮
+                    </span>
+                    &nbsp; and hanging out with friends &amp; family &nbsp;
+                    <span role="img" aria-label="friends">
+                        👨‍👦‍👦.
+                    </span>
+                    I also am a Travel Vlogger.
+                </p>
+            </div>
+        </div>
         </div>
     );
 };
